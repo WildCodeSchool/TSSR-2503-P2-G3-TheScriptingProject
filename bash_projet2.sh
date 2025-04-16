ee033f7bed65e5ee6c8806effdc327c25733595a
Brendan Borne <brendan.borne@hotmail.fr>
2025-04-16 17:30:00 +0200

    Fin d'implémentation logging

--- bash_projet2.sh
+++ bash_projet2.sh
@@ -4,6 +4,7 @@
 #           FONCTIONS
 # ------------------------------ #
 
+
 function action_utilisateur_local() 
 {
     #Fonction 1 :
@@ -20,32 +21,41 @@ function action_utilisateur_local()
     read -r choix
     case $choix in
         1) 
-        echo "Client où créer l'utilisateur :"
-        read -r client
-        echo "Merci de donner un nom d'utilisateur"
-        read -r nom_user
-        ssh $client adduser "$nom_user" ;;
+            echo "Client où créer l'utilisateur :"
+            read -r client
+            echo "Merci de donner un nom d'utilisateur"
+            read -r nom_user
+            ssh $client adduser "$nom_user"
+            log_events "ActionCreerUtilisateur"
+            ;;
         2) 
-        echo "Client où modifier le mot de passe :"
-        read -r client
-        echo "Merci de donner le nom d'utilisateur a qui vous voulez changer de mot de passe"
-        read -r nom_passwd
-        ssh $client passwd "$nom_passwd" ;;
+            echo "Client où modifier le mot de passe :"
+            read -r client
+            echo "Merci de donner le nom d'utilisateur a qui vous voulez changer de mot de passe"
+            read -r nom_passwd
+            ssh $client passwd "$nom_passwd"
+            log_events "ActionModifierMDP"
+            ;;
         3) 
-        echo "Client où supprimer un utilisateur :"
-        read -r client
-        echo "Merci de donner l'utilisateur que vous souhaitez supprimer"
-        read -r user_del
-        ssh $client deluser "$user_del" ;;
+            echo "Client où supprimer un utilisateur :"
+            read -r client
+            echo "Merci de donner l'utilisateur que vous souhaitez supprimer"
+            read -r user_del
+            ssh $client deluser "$user_del"
+            log_events "ActionSupprimerUtilisateur"
+            ;;
         4)
-        echo "Client où désactiver un compte utilisateur :"
-        read -r client
-        echo "Merci de donner l'utilisateur que vous voulez désactiver"
-        read -r user_desactive
-        ssh $client usermod -L "$user_desactive" ;;
+            echo "Client où désactiver un compte utilisateur :"
+            read -r client
+            echo "Merci de donner l'utilisateur que vous voulez désactiver"
+            read -r user_desactive
+            ssh $client usermod -L "$user_desactive"
+            log_events "ActionDesactiverUtilisateur"
+            ;;
         #chage -E 0 username
     esac
 }
+
 
 function action_groupe_local()
 {
@@ -59,23 +69,28 @@ function action_groupe_local()
     read -r choix_groupe_local 
     case $choix_groupe_local in 
         1)
-        echo "Client de l'utilisateur à ajouter à un groupe ?"
-        read -r client
-        echo "Quel utilisateur voulez-vous ajouter à un groupe ?"
-        read -r userName
-        echo "A quel groupe voulez-vous ajouter l'utilisateur ?"
-        read -r group_add
-        ssh $client usermod -a -G $group_add $userName ;; 
+            echo "Client de l'utilisateur à ajouter à un groupe ?"
+            read -r client
+            echo "Quel utilisateur voulez-vous ajouter à un groupe ?"
+            read -r userName
+            echo "A quel groupe voulez-vous ajouter l'utilisateur ?"
+            read -r group_add
+            ssh $client usermod -a -G $group_add $userName 
+            log_events "ActionAjouterUtilisateurGroupe"
+            ;; 
         2)
-        echo "Client de l'utilisateur à sortir d'un groupe ?"
-        read -r client
-        echo "Quel utilisateur voulez-vous sortir d'un groupe ?"
-        read -r userName
-        echo "De quel groupe voulez-vous sortir l'utilisateur ?"
-        read -r group_out
-        ssh $client usermod -r -G $group_out $userName ;;
+            echo "Client de l'utilisateur à sortir d'un groupe ?"
+            read -r client
+            echo "Quel utilisateur voulez-vous sortir d'un groupe ?"
+            read -r userName
+            echo "De quel groupe voulez-vous sortir l'utilisateur ?"
+            read -r group_out
+            ssh $client usermod -r -G $group_out $userName
+            log_event "ActionSupprimerUtilisateurGroupe"
+            ;;
     esac
 }
+
 
 function action_shut()
 {
@@ -91,19 +106,25 @@ function action_shut()
     read -r choix_shut
     case $choix_shut in
         1)
-        echo "Quel client voulez-vous arrêter ?"
-        read -r client
-        ssh $client poweroff ;; 
+            echo "Quel client voulez-vous arrêter ?"
+            read -r client
+            ssh $client poweroff
+            log_events "ActionEteindre"
+            ;; 
         2)
-        echo "Quel client voulez-vous redémarrer ?"
-        read -r client
-        ssh $client reboot ;; 
+            echo "Quel client voulez-vous redémarrer ?"
+            read -r client
+            ssh $client reboot
+            log_events "ActionRedemarrer"
+            ;; 
         3) 
-        echo "Quel client voulez-vous déconnecter ?"
-        read -r client
-        echo "Quel utilisateur voulez-vous déconnecter ?"
-        read -r user
-        ssh $client pkill -KILL -u $user ;;
+            echo "Quel client voulez-vous déconnecter ?"
+            read -r client
+            echo "Quel utilisateur voulez-vous déconnecter ?"
+            read -r user
+            ssh $client pkill -KILL -u $user
+            log_events "ActionDeconnecter"
+            ;;
     esac
 }
 
@@ -113,8 +134,10 @@ function action_update()
     # - Mise-à-jour du système 
     echo "Quel client voulez-vous mettre à jour ?"
     read -r client
-    ssh $client apt update && apt upgrade -y 
+    ssh $client apt update && apt upgrade -y
+    log_events "ActionMiseAJour"
 }
+
 
 function action_repertoire()
 {   
@@ -130,27 +153,34 @@ function action_repertoire()
     read -r choix_repertoire
     case $choix_repertoire in 
         1)
-        echo "Sur quel client voulez-vous créer un répertoire ?"
-        read -r client
-        echo "Comment voulez vous nommer le repertoire ?"
-        read -r name_dir
-        ssh $client mkdir "$name_dir" ;;
+            echo "Sur quel client voulez-vous créer un répertoire ?"
+            read -r client
+            echo "Comment voulez vous nommer le repertoire ?"
+            read -r name_dir
+            ssh $client mkdir "$name_dir"
+            log_events "ActionCreerDossier"
+            ;;
         2) 
-        echo "Sur quel client voulez-vous modifier un répertoire ?"
-        read -r client
-        echo "Quel repertoire voulez vous modifier ?"
-        read -r mod_dir
-        echo "Quel est le nouveau nom ?"
-        read -r new_dir
-        ssh $client mv "$mod_dir" "$new_dir" ;; 
+            echo "Sur quel client voulez-vous modifier un répertoire ?"
+            read -r client
+            echo "Quel repertoire voulez vous modifier ?"
+            read -r mod_dir
+            echo "Quel est le nouveau nom ?"
+            read -r new_dir
+            ssh $client mv "$mod_dir" "$new_dir"
+            log_events "ActionModifDossier"
+            ;; 
         3)
-        echo "Sur quel client voulez-vous supprimer un répertoire ?"
-        read -r client 
-        echo "Quel repertoire voulez vous supprimer ?"
-        read -r dir_del
-        ssh $client rmdir "$dir_del" ;;
+            echo "Sur quel client voulez-vous supprimer un répertoire ?"
+            read -r client 
+            echo "Quel repertoire voulez vous supprimer ?"
+            read -r dir_del
+            ssh $client rmdir "$dir_del"
+            log_events "ActionSupprimerDossier"
+            ;;
     esac
 }
+
 
 function action_prise_en_main()
 {   
@@ -159,7 +189,9 @@ function action_prise_en_main()
     echo "Sur quel ordinateur voulez-vous prendre la main ?"
     read -r client
     ssh $client
+    log_events "ActionPriseEnMain"
 }
+
 
 function action_pare_feu()
 {
@@ -190,11 +222,13 @@ function action_pare_feu()
             echo "Entrez adresse à autoriser"
             read -r adresse
             ssh $client ufw allow from $adresse
+            log_events "ActionAutorPareFeu"
             ;;
         2)
             echo "Entrez adresse à interdire"
             read -r adresse
             ssh $client ufw deny from $adresse
+            log_events "ActionSupprPareFeu"
             ;;
         esac
         ;;
@@ -203,15 +237,18 @@ function action_pare_feu()
         read -r client
         echo "Activation du pare-feu"
         ssh $client ufw enable
+        log_events "ActionActiverPareFeu"
         ;;
     3)
         echo "Sur quel client voulez-vous désactiver le pare-feu ?"
         read -r client
         echo "Désactivation du pare-feu"
         ssh $client ufw disable
+        log_events "ActionDesactiverPareFeu"
         ;;
     esac
 }
+
 
 function action_logiciel()
 {
@@ -235,20 +272,24 @@ function action_logiciel()
         echo "Quel logiciel souhaitez-vous installer ?"
         read -r logiciel
         ssh $client apt install $logiciel
+        log_events "ActionInstallApp"
         ;;
     2)
         echo "Remove"
         echo "Quel logiciel souhaitez-vous supprimer ?"
         read -r logiciel
         ssh $client apt remove $logiciel
+        log_events "ActionSupprApp"
         ;;
     3)
         echo "Quel script souhaitez-vous lancer ?"
         read -r script
         ssh $client bash $script 
+        log_events "ActionLancerScript"
         ;;
     esac
 }
+
 
 function info_compte()
 {
@@ -272,13 +313,22 @@ function info_compte()
     read -r choix
     case $choix in
         1) 
-            ssh $client last $user | head -n 1
+            command=$(ssh $client last $user | head -n 1)
+            echo $command
+            log_info "$client"_"$user" "$command"
+            log_events "InfoDateConnexion"
             ;;
         2)
-            ssh $client passwd $user -S
+            command=$(ssh $client passwd $user -S)
+            echo $command
+            log_info "$client"_"$user" "$command"
+            log_events "InfoDateModifMDP"
             ;;
         3)
-            ssh $client last $user
+            command=$(ssh $client last $user)
+            echo $command
+            log_info "$client"_"$user" "$command"
+            log_events "InfoSessions"
             ;;
     esac
 
@@ -301,10 +351,16 @@ function info_groupe()
     read -r choix
     case $choix in
         1)
-            ssh $client groups $user
+            command=$(ssh $client groups $user)
+            echo $command
+            log_info "$client"_"$user" "$command"
+            log_events "InfoGroupeUtilisateur"
             ;;
         2)
-            ssh $client cat /home/$user/.bash_history
+            command=$(ssh $client cat /home/$user/.bash_history)
+            echo $command
+            log_info "$client"_"$user" "$command"
+            log_events "InfoHistoriqueUtilisateur"
             ;;
     esac
 }
@@ -317,24 +373,16 @@ function info_droits()
     # - Droits/permissions de l’utilisateur sur un fichier
     echo "Info droits fichier/dossier"
     echo "--------------------"
-    echo "Sur quel client récupérer vos informations ?"
+    echo "Sur quel client recuperer vos informations ?"
     read -r client
-    echo "Sur quel utilisateur récupérer vos informations ?"
+    echo "Sur quel utilisateur recuperer vos informations ?"
     read -r user
-    echo "Sur quel dossier récupérer vos informations ?"
+    echo "Sur quel dossier/fichier recuperer vos informations ?"
     read -r target
-    echo "Que voulez-vous savoir ?"
-    echo "1) Droits de $user sur dossier $target"
-    echo "2) Droits de $user sur fichier $target"
-    read -r choix
-    case $choix in
-        1) 
-            ssh $client getfacl $target 
-            ;;
-        2)
-            ssh $client getfacl $target 
-            ;;
-    esac
+    command=$(ssh $client getfacl $target)
+    echo $command
+    log_info "$client"_"$user" "$command"
+    log_events "InfoDroitsRepertoire"
 }
 
 function info_os_version()
@@ -345,8 +393,8 @@ function info_os_version()
     read -r client
     command=$(ssh $client cat /etc/os-release | grep "PRETTY_NAME" | cut -d = -f 2 | tr -d '"')
     echo $command
-    log_events "InfoOS"
     log_info $client-GEN "$command"
+    log_events "InfoOS"
 }
 
 function info_disk_number()
@@ -367,10 +415,15 @@ function info_disk_number()
     1)
         amount=$(ssh $client hwinfo --disk --short | wc -l)
         amount=$(($amount-1))
-        echo "Nombre de disques : $amount"
+        command=$(echo "Nombre de disques : $amount")
+        log_info $client-GEN "$command"
+        log_events "InfoNombreDisques"
         ;;
     2)
-        ssh $client lsblk -f
+        command=$(ssh $client lsblk -f)
+        echo $command
+        log_info $client-GEN "$command"
+        log_events "InfoPartitions"
         ;;
     esac
 }
@@ -386,13 +439,22 @@ function info_app()
     read -r choix_app
     case $choix_app in 
         1) 
-            ssh $client apt --installed list
+            command=$(ssh $client apt --installed list)
+            echo $command
+            #log_info $client-GEN "$command"
+            #log_events "InfoApplisInstallées"
             ;;
         2)
-            ssh $client systemctl
+            command=$(ssh $client systemctl)
+            echo $command
+            #log_info $client-GEN "$command"
+            #log_events "InfoServicesEnCours"
             ;;
         3)
-            ssh $client cut -d: -f1 /etc/passwd
+            command=$(ssh $client cut -d: -f1 /etc/passwd)
+            echo $command
+            #log_info $client-GEN "$command"
+            #log_events "InfoUtilisateursLocaux"
             ;;
     esac
 }
@@ -417,20 +479,34 @@ function info_computer()
     read -r choix
     case $choix in 
         1) 
-            ssh $client lscpu
+            command=$(ssh $client lscpu)
+            echo $command
+            log_info $client-GEN "$command"
+            log_events "InfoCPU"
             ;;
         2) 
-            ssh $client free
+            command=$(ssh $client free)
+            echo $command
+            log_info $client-GEN "$command"
+            log_events "InfoRAM"
             ;;
         3) 
-            ssh $client free
+            command=$(ssh $client free)
+            echo $command
+            log_info $client-GEN "$command"
+            log_events "InfoRAM"
             ;;
         4)
-            ssh $client df
+            command=$(ssh $client df)
+            echo $command
+            log_info $client-GEN "$command"
+            log_events "InfoUtilisationDisque"
             ;;
         5)
-            #sudo apt install htop
-            ssh $client htop
+            command=$(ssh $client htop)
+            echo $command
+            log_info $client-GEN "$command"
+            log_events "InfoUtilisationCPU"
             ;;
     esac
 }
@@ -445,20 +521,17 @@ function info_search()
     echo "2) Rechercher des évenements dans le fichier log_evt.log pour un ordinateur"
     case $choix_search in 
         1)
-            echo "Quel évenement recherchez vous ?"
+            echo "Quel utilisateur recherchez vous ?"
             read -r event_user
             cat log_evt.log | grep $event_user
             ;;
         2)
-            echo "Quel évenement recherchez vous ?"
+            echo "Quel ordinateur recherchez vous ?"
             read -r event_computer
             cat log_evt.log | grep $event_computer
             ;;
     esac
 }
-
-
-# LOGS
 
 function log_events()
 {
@@ -486,14 +559,15 @@ function log_info()
     echo "$logRes" >> log/"$logFile"
 }
 
-# FONCTIONS INFO A LOGGER AUSSI
-
 # ------------------------------ #
 #           EXECUTION
 # ------------------------------ #
 
+# On inscrit dans les logs le début de l'exécution du script
+log_events "********StartScript********"
 # Variable pour gérer l'arrêt
 run=1
+# On lance la boucle
 while [ $run -eq 1 ]
 do
     #clear
@@ -529,7 +603,7 @@ do
                             action_groupe_local
                             ;;
                         q)
-                            exit 0
+                            break
                             ;;
                     esac
                     ;;
@@ -565,12 +639,12 @@ do
                             action_logiciel
                             ;;
                         q)
-                            exit 0
+                            break
                             ;;
                     esac
                     ;;
                 q)
-                    exit 0
+                    break
                     ;;
             esac
             ;;
@@ -603,7 +677,7 @@ do
                             info_droits
                             ;;
                         q)
-                            exit 0
+                            break
                             ;;
                     esac
                     ;;
@@ -613,8 +687,9 @@ do
                     echo "--------------------"
                     echo "1) Informations OS"
                     echo "2) Informations disques"
-                    echo "3) Informations matériel"
-                    echo "4) Recherche dans les logs"
+                    echo "3) Informations applications"
+                    echo "4) Informations matériel"
+                    echo "5) Recherche dans les logs"
                     echo "q) Quitter"
                     read -r choix3
                     case $choix3 in
@@ -625,23 +700,29 @@ do
                             info_disk_number
                             ;;
                         3)
-                            info_computer
+                            info_app
                             ;;
                         4)
+                            info_computer
+                            ;;
+                        5)
                             info_search
                             ;;
                         q)
-                            exit 0
+                            break
                             ;;
                     esac
                     ;;
                 q)
-                    exit 0
+                    break
                     ;;
             esac
             ;;
         q)
-            exit 0
+            break
             ;;
     esac
 done
+
+# On inscrit dans les logs la fin de l'exécution du script
+log_events "********EndScript********"
